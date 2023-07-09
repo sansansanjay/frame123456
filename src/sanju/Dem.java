e820dd22cb85c891912ddd366fff7d42bc44008a
sansa <sansa@Sanjaykumar>
2023-07-09 22:30:34 +0530

    nnnnnnnnnnnnnnnn

---
+++ src/sanju/Dem.java
@@ -0,0 +1,30 @@
+package sanju;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+
+public class Dem
+{
+	
+
+public static void main(String[] args)
+{
+	String s="sanju";
+	String d="saaa";
+	String h="dady";
+	System.out.println(s.concat(h+d));
+	
+  
+	
+	
+	 
+	 
+		 
+		
+	
+		
+}}
+
